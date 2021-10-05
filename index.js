a64c2c7cb5f88d8b5890911365d964cd54abcd5b
Aravinth S <80613214+ambiaravinth@users.noreply.github.com>
2021-10-05 09:02:23 +0530

    Add files via upload

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
 var http=require("http");
+const PORT = process.env.PORT || 8000;
+
 
 var server = http.createServer(function(request,response)
 {
@@ -6,5 +8,5 @@ var server = http.createServer(function(request,response)
     response.write(" <h1><h1 style=\"text-align: center;\">Here the Responce!!!</h1> <br> <p1>This is from localhost:8000 Responce...</p1><br><br> <img src=\"https://c.tenor.com/1IWU8bZl2bQAAAAC/welcome-back-back.gif\" alt=\"Not Available\" >   ");
     console.log("1 Request received");
 });
-server.listen(8000);
+server.listen(PORT);
 console.log("server running");
